f5b79e459c379f01a49667828b5061a3f65ff7d9
Juicer149 <karlal_bin_jimmy@proton.me>
2025-09-14 13:00:27 +0200

    Förenklade tema med endast toggle mellan ljust och mörkt, fixade även lsp för när man använder venv

--- init.lua
+++ init.lua
@@ -1,16 +1,94 @@
 -- Sätt leader till mellanslag
 vim.g.mapleader = " "
 
--- Ladda konfiguration
+-- ############################################################
+-- # Tema-konfiguration (ändra här för att byta dark/light tema)
+-- ############################################################
+local dark_theme = {
+  name = "rose-pine",
+  setup = function()
+    require("rose-pine").setup({ variant = "moon" })
+  end,
+}
+local light_theme = {
+  name = "dayfox",
+  setup = function()
+    -- dayfox (Nightfox-tema) kräver ingen extra setup
+  end,
+}
+
+-- ############################################################
+-- # Ladda konfiguration
+-- ############################################################
 require("core.options")
 require("core.keymaps")
 require("core.autocmds")
 require("core.commands")
 require("plugins")
 
--- Ladda teman och färgscheman
-require("core.theme")
+-- ############################################################
+-- # Tema-hantering: autoload + kommandon
+-- ############################################################
+local theme_file = vim.fn.stdpath("config") .. "/theme.txt"
 
--- Ladda plugins
+local function save_theme(mode)
+  local f = io.open(theme_file, "w")
+  if f then
+    f:write(mode)
+    f:close()
+  end
+end
+
+vim.api.nvim_create_user_command("Darkmode", function()
+  dark_theme.setup()
+  vim.cmd.colorscheme(dark_theme.name)
+  save_theme("dark")
+  print("🌙 Dark mode aktiverat (" .. dark_theme.name .. ")")
+end, {})
+
+vim.api.nvim_create_user_command("Lightmode", function()
+  light_theme.setup()
+  vim.cmd.colorscheme(light_theme.name)
+  save_theme("light")
+  print("☀️ Light mode aktiverat (" .. light_theme.name .. ")")
+end, {})
+
+vim.api.nvim_create_user_command("ToggleTheme", function()
+  local mode = "light"
+  local f = io.open(theme_file, "r")
+  if f then
+    mode = f:read("*l") or "light"
+    f:close()
+  end
+  if mode == "dark" then
+    vim.cmd("Lightmode")
+  else
+    vim.cmd("Darkmode")
+  end
+end, {})
+
+-- Autoload senaste använda tema
+do
+  local f = io.open(theme_file, "r")
+  if f then
+    local mode = f:read("*l")
+    f:close()
+    if mode == "dark" then
+      dark_theme.setup()
+      vim.cmd.colorscheme(dark_theme.name)
+    elseif mode == "light" then
+      light_theme.setup()
+      vim.cmd.colorscheme(light_theme.name)
+    end
+  else
+    -- fallback: ljus som standard
+    light_theme.setup()
+    vim.cmd.colorscheme(light_theme.name)
+  end
+end
+
+-- ############################################################
+-- # LSP
+-- ############################################################
 require("core.lsp")
 
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,11 +1,5 @@
 local map = vim.keymap.set
 
--- Flytta rader
-map("n", "<A-j>", ":m .+1<CR>==", { desc = "Flytta raden nedåt" })
-map("n", "<A-k>", ":m .-2<CR>==", { desc = "Flytta raden uppåt" })
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Flytta markerad rad nedåt" })
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Flytta markerad rad uppåt" })
-
 -- jk som Escape
 map("i", "jk", "<Esc>", { desc = "jk som Escape i insert-läge" })
 map("v", "jk", "<Esc>", { desc = "jk som Escape i visual-läge" })
@@ -14,6 +8,10 @@ map("v", "jk", "<Esc>", { desc = "jk som Escape i visual-läge" })
 map("n", "<leader>w", ":w<CR>", { desc = "Spara fil" })
 map("n", "<leader>q", ":q<CR>", { desc = "Stäng fil" })
 
--- Tab-navigering med Space+g och Space+f
+-- Tab-navigering
 map("n", "<leader>g", "gt", { desc = "Nästa tab" })
 map("n", "<leader>f", "gT", { desc = "Föregående tab" })
+
+-- Tema-växling
+map("n", "<leader>t", ":ToggleTheme<CR>", { desc = "Växla mellan Darkmode/Lightmode" })
+
