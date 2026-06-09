8c92435e4b4ab687d067066231901365832fafeb
juicer149 <karlal_bin_jimmy@proton.me>
2026-06-09 13:35:56 +0200

    upgrade neovim and fix treesitter setup

--- config/lua/plugins/treesitter.lua
+++ config/lua/plugins/treesitter.lua
@@ -1,36 +1,40 @@
+local parsers = {
+  "python",
+  "lua",
+  "vim",
+  "vimdoc",
+  "bash",
+  "json",
+  "yaml",
+  "toml",
+  "html",
+  "css",
+  "markdown",
+  "markdown_inline",
+  "c",
+  "cpp",
+  "go",
+  "nim",
+  "regex",
+  "query",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
 
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "python",
-        "lua",
-        "vim",
-        "vimdoc",
-        "bash",
-        "json",
-        "yaml",
-        "toml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "c",
-        "cpp",
-        "go",
-        "nim",
-        "regex",
-        "query",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
